bda39018c1018045b774adeff2338d41e6c9dfc4
Jeremy <jeremy.cohen@edu.ece.fr>
2017-12-07 00:44:05 +0100

    First Commit

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -6,8 +6,8 @@
 using CppAD::AD;
 
 // TODO: Set the timestep length and duration
-size_t N = 0;
-double dt = 0;
+size_t N = 10;
+double dt = 0.1;
 
 // This value assumes the model presented in the classroom is used.
 //
@@ -20,6 +20,25 @@ double dt = 0;
 //
 // This is the length from front to CoG that has a similar radius.
 const double Lf = 2.67;
+////
+// NOTE: feel free to play around with this
+// or do something completely different
+double ref_v = 80;
+double ref_cte = 0;
+double ref_epsi = 0;
+
+// The solver takes all the state variables and actuator
+// variables in a singular vector. Thus, we should to establish
+// when one variable starts and another ends to make our lifes easier.
+size_t x_start = 0;
+size_t y_start = x_start + N;
+size_t psi_start = y_start + N;
+size_t v_start = psi_start + N;
+size_t cte_start = v_start + N;
+size_t epsi_start = cte_start + N;
+size_t delta_start = epsi_start + N;
+size_t a_start = delta_start + N - 1;
+
 
 class FG_eval {
  public:
@@ -29,11 +48,93 @@ class FG_eval {
 
   typedef CPPAD_TESTVECTOR(AD<double>) ADvector;
   void operator()(ADvector& fg, const ADvector& vars) {
+
     // TODO: implement MPC
     // `fg` a vector of the cost constraints, `vars` is a vector of variable values (state & actuators)
     // NOTE: You'll probably go back and forth between this function and
     // the Solver function below.
+
+    // The cost is stored is the first element of `fg`.
+    // Any additions to the cost should be added to `fg[0]`.
+    fg[0] = 0;
+
+    // Reference State Cost
+    // TODO: Define the cost related the reference state and
+    // any anything you think may be beneficial. 
+    for (int t = 0; t<N;t++){
+      fg[0] += 2000*CppAD::pow(vars[cte_start + t] - ref_cte,2);
+      fg[0] += 2000*CppAD::pow(vars[epsi_start +t] - ref_epsi,2);
+      fg[0] += CppAD::pow(vars[v_start +t] - ref_v,2);
+    }
+    for (int t =0; t<N-1;t++){
+      fg[0]+= 5*CppAD::pow(vars[delta_start + t],2);
+      fg[0]+= 5*CppAD::pow(vars[a_start + t],2);
+    }
+
+    for(int t=0; t<N-2;t++){
+     // fg[0] += CppAD::pow(vars[delta_start +t +1] - vars[delta_start+t],2);
+      fg[0] += 10*CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
+      fg[0] += 200 * CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2); // Smoother transition
+    }
+    //
+    // Setup Constraints
+    //
+    // NOTE: In this section you'll setup the model constraints.
+
+    // Initial constraints
+    //
+    // We add 1 to each of the starting indices due to cost being located at
+    // index 0 of `fg`.
+    // This bumps up the position of all the other values.
+    fg[1 + x_start] = vars[x_start];
+    fg[1 + y_start] = vars[y_start];
+    fg[1 + psi_start] = vars[psi_start];
+    fg[1 + v_start] = vars[v_start];
+    fg[1 + cte_start] = vars[cte_start];
+    fg[1 + epsi_start] = vars[epsi_start];
+
+    // The rest of the constraints
+    for (int t = 0; t < N-1; t++) {
+
+      // Time T+1
+      AD<double> x1 = vars[x_start + t+1];
+      AD<double> y1 = vars[y_start +t+1];
+      AD<double> psi1 = vars[psi_start + t+1];
+      AD<double> v1 = vars[v_start + t+1];
+      AD<double> cte1 = vars[cte_start + t+1];
+      AD<double> epsi1 = vars[epsi_start + t+1];
+      
+      //Time T
+      AD<double> x0 = vars[x_start + t ];
+      AD<double> y0 = vars[y_start + t ];
+      AD<double> psi0 = vars[psi_start + t];
+      AD<double> v0 = vars[v_start + t ];
+      AD<double> cte0 = vars[cte_start + t];
+      AD<double> epsi0 = vars[epsi_start + t];
+
+      AD<double> delta0 = vars[delta_start + t];
+      AD<double> a0 = vars[a_start + t];
+
+      AD<double> f0 = coeffs[0] + coeffs[1] * x0 + coeffs[2]*x0*x0 + coeffs[3]*x0*x0*x0;
+      AD<double> psides0 = CppAD::atan(coeffs[1] +2*coeffs[2]*x0 + 3*coeffs[3]*x0*x0);
+
+      // Here's `x` to get you started.
+      // The idea here is to constraint this value to be 0.
+      //
+      // NOTE: The use of `AD<double>` and use of `CppAD`!
+      // This is also CppAD can compute derivatives and pass
+      // these to the solver.
+
+      // TODO: Setup the rest of the model constraints
+      fg[2 + x_start + t] = x1 - (x0 + v0 * CppAD::cos(psi0) * dt);
+      fg[2 + y_start + t] = y1 - (y0 + v0 * CppAD::sin(psi0) * dt);
+      fg[2 + psi_start + t] = psi1 - (psi0 - v0 * delta0 / Lf * dt);
+      fg[2 + v_start + t] = v1 - (v0 + a0 * dt);
+      fg[2 + cte_start + t] = cte1 - ((f0 - y0) + (v0 * CppAD::sin(epsi0) * dt));
+      fg[2 + epsi_start + t] = epsi1 - ((psi0 - psides0) - v0 * delta0 / Lf * dt); 
+    }
   }
+
 };
 
 //
@@ -44,7 +145,6 @@ MPC::~MPC() {}
 
 vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
   bool ok = true;
-  size_t i;
   typedef CPPAD_TESTVECTOR(double) Dvector;
 
   // TODO: Set the number of model variables (includes both states and inputs).
@@ -52,9 +152,9 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
   // element vector and there are 10 timesteps. The number of variables is:
   //
   // 4 * 10 + 2 * 9
-  size_t n_vars = 0;
+  size_t n_vars = 6*N + 2*(N-1);
   // TODO: Set the number of constraints
-  size_t n_constraints = 0;
+  size_t n_constraints = 6*N;
 
   // Initial value of the independent variables.
   // SHOULD BE 0 besides initial state.
@@ -66,7 +166,18 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
   Dvector vars_lowerbound(n_vars);
   Dvector vars_upperbound(n_vars);
   // TODO: Set lower and upper limits for variables.
-
+  for (int i=0; i<delta_start;i++){
+    vars_lowerbound[i]= -1e19;
+    vars_upperbound[i] = 1e19;
+  }
+  for (int i=delta_start; i<a_start;i++){
+    vars_lowerbound[i]= -0.436332*Lf;
+    vars_upperbound[i] = 0.436332*Lf;
+  }
+  for (int i=a_start; i<n_vars;i++){
+    vars_lowerbound[i]= -1.0;
+    vars_upperbound[i] = 1.0;
+  }
   // Lower and upper limits for the constraints
   // Should be 0 besides initial state.
   Dvector constraints_lowerbound(n_constraints);
@@ -75,6 +186,19 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
     constraints_lowerbound[i] = 0;
     constraints_upperbound[i] = 0;
   }
+  constraints_lowerbound[x_start] = state[0];
+  constraints_lowerbound[y_start] = state[1];
+  constraints_lowerbound[psi_start] = state[2];
+  constraints_lowerbound[v_start] = state[3];
+  constraints_lowerbound[cte_start] = state[4];
+  constraints_lowerbound[epsi_start] = state[5];
+
+  constraints_upperbound[x_start] = state[0];
+  constraints_upperbound[y_start] = state[1];
+  constraints_upperbound[psi_start] = state[2];
+  constraints_upperbound[v_start] = state[3];
+  constraints_upperbound[cte_start] =state[4];
+  constraints_upperbound[epsi_start] = state[5];
 
   // object that computes objective and constraints
   FG_eval fg_eval(coeffs);
@@ -114,8 +238,17 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
 
   // TODO: Return the first actuator values. The variables can be accessed with
   // `solution.x[i]`.
+    vector<double> result;
+    result.push_back(solution.x[delta_start]);
+    result.push_back(solution.x[a_start]);
+
+    // Add "future" solutions (where MPC is going)
+    for (int i = 0; i < N-1; ++i) {
+        result.push_back(solution.x[x_start + i+1]);
+        result.push_back(solution.x[y_start + i+1]);
+    }
   //
   // {...} is shorthand for creating a vector, so auto x1 = {1.0,2.0}
   // creates a 2 element double vector.
-  return {};
+  return result;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -91,6 +91,36 @@ int main() {
           double py = j[1]["y"];
           double psi = j[1]["psi"];
           double v = j[1]["speed"];
+          double steer_value = j[1]["steering_angle"];
+          double throttle_value = j[1]["throttle"];
+
+          // DO TRANSFORMATION & ROTATION IN ORDER TO GET GOOD COORDINATES
+          // WE FOUND OURSELVES WITH X=0,Y=0,PSI=0
+
+          for(int i =0; i<ptsx.size();i++)
+          {
+            double shift_x = ptsx[i] - px;
+            double shift_y = ptsy[i] - py;
+            ptsx[i] = shift_x*cos(-psi) - shift_y*sin(-psi);
+            ptsy[i] = shift_x*sin(-psi) + shift_y*cos(-psi);
+          }
+          // MAKE A VECTOR
+          double* ptrx = &ptsx[0];
+          Eigen::Map<Eigen::VectorXd> ptsx_transform(ptrx,6);
+          
+          double* ptry = &ptsy[0];
+          Eigen::Map<Eigen::VectorXd> ptsy_transform(ptry,6);
+
+          // POLYFIT TO HAVE THE COEFFICIENTS (3 ORDER)
+          auto coeffs = polyfit(ptsx_transform,ptsy_transform,3);
+          
+          // Calculate CTE and EPSI 
+          double cte = polyeval(coeffs,0); // O because shifted
+          double epsi = - atan(coeffs[1]);
+
+
+          Eigen::VectorXd state(6);
+          state <<0,0,0,v,cte,epsi;
 
           /*
           * TODO: Calculate steering angle and throttle using MPC.
@@ -98,35 +128,51 @@ int main() {
           * Both are in between [-1, 1].
           *
           */
-          double steer_value;
-          double throttle_value;
+          auto vars = mpc.Solve(state, coeffs);
+         
+         //Display the waypoints/reference line
+         // The Yellow Line
+          vector<double> next_x_vals;
+          vector<double> next_y_vals;
+          double poly_inc = 2.5;
+          int num_points = 25;
+          for (int i=1;i<num_points;i++){
+            next_x_vals.push_back(poly_inc*i);
+            next_y_vals.push_back(polyeval(coeffs,poly_inc*i));
+          }
+
+          //Display the MPC predicted trajectory 
+          //The Green Line
+          vector<double> mpc_x_vals;
+          vector<double> mpc_y_vals;
+          for (int i=2;i<vars.size();i++){
+            if(i%2 ==0){
+              mpc_x_vals.push_back(vars[i]);
+            }
+            else{
+              mpc_y_vals.push_back(vars[i]);
+            }
+          }
 
           json msgJson;
+          double Lf = 2.67;
           // NOTE: Remember to divide by deg2rad(25) before you send the steering value back.
           // Otherwise the values will be in between [-deg2rad(25), deg2rad(25] instead of [-1, 1].
+          steer_value = vars[0]/(deg2rad(25)*Lf);
+          throttle_value = vars[1];
           msgJson["steering_angle"] = steer_value;
           msgJson["throttle"] = throttle_value;
 
-          //Display the MPC predicted trajectory 
-          vector<double> mpc_x_vals;
-          vector<double> mpc_y_vals;
-
           //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
           // the points in the simulator are connected by a Green line
-
           msgJson["mpc_x"] = mpc_x_vals;
           msgJson["mpc_y"] = mpc_y_vals;
-
-          //Display the waypoints/reference line
-          vector<double> next_x_vals;
-          vector<double> next_y_vals;
 
           //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
           // the points in the simulator are connected by a Yellow line
 
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
-
 
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
           std::cout << msg << std::endl;
